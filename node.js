eb64651bcfe8e6e89812704d538249631dcb1a73
Nishitasarker <shuvrodevsaikat71@gmail.com>
2026-02-25 23:12:22 +0600

    i can't solve this

--- node.js
+++ node.js
@@ -65,7 +65,9 @@ selected.classList.add('bg-blue-800','text-white');
 
 
 mainContainer.addEventListener('click', function(event){
-                       event.target.closest('.job-card');
+    
+console.log(event.target.classList.contains('interview-btn'))
+    if(event.target.classList.contains('interview-btn')){
     const parentNode = event.target.parentNode.parentNode;
     const jobTitle = parentNode.querySelector('.job-title').innerText;
     const jobType = parentNode.querySelector('.job-type').innerText;
@@ -84,6 +86,9 @@ const jobTitleExist = interviewList.find(item=> item.jobTitle == cardInfo.jobTit
 if(!jobTitleExist){
     interviewList.push(cardInfo);
 }
+renderThriving()
+
+ }
 })
 
 
@@ -91,9 +96,31 @@ function renderThriving(){
     filterSection.innerHTML='';
 
 
-    for(let Thrive of renderThriving){
+    for(let interview of interviewList){
+        console.log(interview);
+        
         let div=document.createElement('div');
-        div.className=``
-        div.innerHTML=``
+        div.className=`job-card bg-white pt-4 pl-2 items-start  mb-4 shadow-sm flex justify-between`
+        div.innerHTML=`<div>
+    <h3 class="job-title  font-bold text-xl mb-4 pt-5">
+    Mobile First Corp
+   </h3> 
+   <p class="job-type text-gray-400 font-bold m-0 py-0">React Native Developer</p>
+   <ol class="job-details-list text-gray-400 pt-6 pb-3">
+    Remote
+    <li>Full-time</li>
+    <li>$130,000-$175,000</li>
+   </ol>
+   <!-- <button class="bg-gray-400 text-blue-100 font-semibold py-2 px-3 rounded-sm shadow">NOT APPLIED</button> -->
+   <p class="text pt-3">Build cross-platform mobile applications using React Native.Work on products used by millions of users worldwide.</p>
+<div class="flex gap-5 py-3">
+     <button class="interview-btn border-2 border-green-500 hover:bg-green-300 py-2 px-3 hover rounded-sm shadow text-green-500">INTERVIEW</button>
+   <button class="border-2 border-red-500 hover:bg-red-300 rounded-sm  py-2 px-3 shadow text-red-500">REJECTED</button>
+</div>
+ </div> 
+
+ <div class="w-8 h-8 flex items-center justify-center border border-gray-300 rounded-full ml-4 cursor-pointer mr-8 mt-6">
+      <i class="fa-regular fa-trash-can"></i></div>
+`
     }
 }
